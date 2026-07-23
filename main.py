0aa7586962058b94ad2539154d8e926c629b514d
Neeam Radia <67102613+neeamradia@users.noreply.github.com>
2026-07-23 11:49:19 +0100

    adding API calls

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # monte carlo-based options pricer (european) by Neeam Radia.
 
 import numpy as np
-
+import yfinance as yf
 
 # Basic var definiton:
 strike = 0              # price at which the option allows purchase / sale when it expires.
 s_0 = 0                 # current stock price at time of purchasing/pricing option
 s_t = 0                 # simulated final stock price at expiry
-r = 0      # risk-free rate of growth - assumed to be UK gilt yield.
+r = r = 0.0415          # UK 1Y gilt yield, BoE yield curve, pulled 2026-07-23    risk-free rate of growth - assumed to be UK gilt yield.
 sigma = 0               # Volatility
 t = 0                   # number of years till expiry
 
@@ -15,7 +15,17 @@ n = 0                   # number of samples drawn for monte-carlo sim.
 
 call_type =            # is the option a call or a put?
 
+tickerInput = input("Enter the ticker of the stock you desire to price the option for.").upper
 
+
+
+###################
+# Pulling data from yfinance API
+ticker = yf.ticker(tickerInput))
+s_0 = ticker.fast_info["lastPrice"]     # pull correct current stock price using API
+
+
+##########################################
 # function
 for i in range n:
     Z[i] = np.random.standard_normal() # n-1 to avoid gates and fenceposts error.
